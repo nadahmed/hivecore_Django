d91cd87c610f68f2ae3bb01386426b769a9b16a3
saydur8853 <saydur15-8853@diu.edu.bd>
2021-09-28 17:59:59 +0600

    cloudinary

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-# import cloudinary_storage
+import cloudinary_storage
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     'cloudinary_storage',
+    'cloudinary',
 
 ]
 
